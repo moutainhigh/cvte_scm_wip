d2a58757c7132fcf663b393167fe750f8fd599e4
薛宇挺 <xueyuting@cvte.com>
2020-05-05 15:33:12 +0800

    测试

---
+++ wip-controller/src/main/java/com/cvte/scm/wip/controller/Bootstrap.java
@@ -0,0 +1,45 @@
+package com.cvte.scm.wip.controller;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableCSBConfig;
+import com.cvte.csb.cache.annotation.EnableCSBRedisCache;
+import com.cvte.csb.cloud.annotation.EnableCSBCloud;
+import com.cvte.csb.core.annotation.EnableCSB;
+import com.cvte.csb.jdbc.mybatis.annotation.EnableCSBMybatis;
+import com.cvte.csb.job.core.EnableCSBJob;
+import com.cvte.csb.jwt.annotation.EnableCSBJwt;
+import com.cvte.csb.log.annotation.EnableCSBLog;
+import com.cvte.csb.msg.annotation.EnableCSBMsg;
+import com.cvte.csb.store.annotation.EnableCSBStore;
+import com.cvte.csb.web.annotation.EnableCSBWeb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @Author: wufeng
+ * @Date: 2019/12/23 12:12
+ */
+@EnableCSB
+@EnableCSBMybatis
+@EnableCSBWeb
+@EnableCSBConfig
+@EnableCSBRedisCache
+@EnableCSBJob
+
+@EnableCSBMsg
+@EnableCSBLog
+@ComponentScan({"com.cvte.scm.demo", "com.cvte.csb.wfp.api.sdk"})
+@EnableCSBJwt
+@EnableAsync
+@EnableCSBCloud
+@EnableFeignClients(basePackages = {"com.cvte.scm.demo.client"})
+@EnableCSBStore
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Bootstrap.class);
+        app.run(args);
+    }
+
+}
---
+++ wip-domain/src/test/java/com/cvte/scm/wip/domain/user/service/SysUserServiceTest.java
@@ -0,0 +1,46 @@
+package com.cvte.scm.wip.domain.user.service;
+
+import com.alibaba.fastjson.JSON;
+import com.cvte.scm.wip.domain.BaseJunitTest;
+import com.cvte.scm.wip.domain.common.user.entity.UserBaseEntity;
+import com.cvte.scm.wip.domain.common.user.repository.UserRepository;
+import com.cvte.scm.wip.domain.common.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Author: wufeng
+ * @Date: 2019/12/25 11:26
+ */
+@Slf4j
+public class SysUserServiceTest extends BaseJunitTest {
+
+    @Autowired
+    private UserService sysUserService;
+
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Before
+//    public void initMock() {
+//        UserBaseEntity mockUser = new UserBaseEntity();
+//        mockUser.setAccount("zhangsan");
+//        mockUser.setAccountType("2");
+//        mockUser.setEmail("test@cvte.com");
+//
+//        UserBaseEntity mockFeignResult = new UserBaseEntity();
+//
+//        Mockito.when(userRepository.getSysUserDetailByAccount(Mockito.any(String.class))).thenReturn(mockFeignResult);
+//    }
+
+
+//    @Test
+    public void feignClientDemo() {
+        String account = "wufeng5300";
+        UserBaseEntity user = sysUserService.getUserByAccount(account);
+        log.info("用户{}身份信息:{}", account, JSON.toJSONString(user));
+    }
+}
