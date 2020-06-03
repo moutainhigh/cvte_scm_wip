5351bb8311f412c838f876c569d4dcd266510e12
薛宇挺 <xueyuting@cvte.com>
2020-06-03 21:17:51 +0800

    [feat][change]待确认列表视图接口

--- wip-controller/src/main/java/com/cvte/scm/wip/controller/requirement/admin/ReqInsController.java
+++ wip-controller/src/main/java/com/cvte/scm/wip/controller/requirement/admin/ReqInsController.java
@@ -2,6 +2,8 @@ package com.cvte.scm.wip.controller.requirement.admin;
 
 import com.cvte.csb.core.interfaces.vo.RestResponse;
 import com.cvte.scm.wip.app.req.confirm.ReqInsConfirmApplication;
+import com.cvte.scm.wip.domain.common.view.vo.SysViewPageParamVO;
+import com.cvte.scm.wip.domain.core.requirement.service.WipReqLinePageService;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,18 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(tags = "待确认列表")
 @RequestMapping("/admin/req/ins")
-public class ReqInsConfirmController {
+public class ReqInsController {
 
     private ReqInsConfirmApplication reqInsConfirmApplication;
+    private WipReqLinePageService wipReqLinePageService;
 
-    public ReqInsConfirmController(ReqInsConfirmApplication reqInsConfirmApplication) {
+    public ReqInsController(ReqInsConfirmApplication reqInsConfirmApplication, WipReqLinePageService wipReqLinePageService) {
         this.reqInsConfirmApplication = reqInsConfirmApplication;
+        this.wipReqLinePageService = wipReqLinePageService;
     }
 
     @PostMapping("/confirm")
     public RestResponse confirm(@RequestBody String... insHeaderId) {
         reqInsConfirmApplication.doAction(insHeaderId);
         return new RestResponse();
+    }
+
+    @PostMapping("/info")
+    public RestResponse reqInsInfo(@RequestBody SysViewPageParamVO sysViewPageParam) {
+        return new RestResponse().setData(wipReqLinePageService.reqInsInfo(sysViewPageParam));
     }
 
 }
