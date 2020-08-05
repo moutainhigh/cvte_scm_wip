844b0d8b02079e20c8e4b7deab978b00e45c09c5
zhongyuan <zhongyuan@cvte.com>
2020-08-05 10:28:54 +0800

    [feat][ckd] 修改调拨校验

--- wip-domain/src/main/java/com/cvte/scm/wip/domain/core/ckd/handler/impl/McTaskConfirmUpdateToRejectHandler.java
+++ wip-domain/src/main/java/com/cvte/scm/wip/domain/core/ckd/handler/impl/McTaskConfirmUpdateToRejectHandler.java
@@ -3,16 +3,15 @@ package com.cvte.scm.wip.domain.core.ckd.handler.impl;
 import com.cvte.csb.core.exception.client.params.ParamsIncorrectException;
 import com.cvte.csb.core.exception.client.params.ParamsRequiredException;
 import com.cvte.csb.toolkit.ObjectUtils;
-import com.cvte.csb.toolkit.StringUtils;
 import com.cvte.scm.wip.domain.core.ckd.annotation.McTaskStatusAnnotation;
 import com.cvte.scm.wip.domain.core.ckd.dto.query.WipMcTaskLineQuery;
 import com.cvte.scm.wip.domain.core.ckd.dto.view.McTaskInfoView;
 import com.cvte.scm.wip.domain.core.ckd.dto.view.WipMcTaskLineView;
-import com.cvte.scm.wip.domain.core.ckd.enums.McTaskDeliveryStatusEnum;
 import com.cvte.scm.wip.domain.core.ckd.enums.McTaskLineStatusEnum;
 import com.cvte.scm.wip.domain.core.ckd.enums.McTaskStatusEnum;
 import com.cvte.scm.wip.domain.core.ckd.handler.McTaskStatusUpdateIHandler;
 import com.cvte.scm.wip.domain.core.ckd.service.WipMcTaskLineService;
+import com.cvte.scm.wip.domain.core.ckd.service.WipMcTaskValidateService;
 import com.cvte.scm.wip.domain.core.ckd.service.WipMcTaskVersionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,10 +31,10 @@ public class McTaskConfirmUpdateToRejectHandler implements McTaskStatusUpdateIHa
 
     @Autowired
     private WipMcTaskVersionService wipMcTaskVersionService;
-
     @Autowired
     private WipMcTaskLineService wipMcTaskLineService;
-
+    @Autowired
+    private WipMcTaskValidateService validateService;
 
     @Override
     public void handler(McTaskInfoView mcTaskInfoView) {
@@ -63,22 +62,12 @@ public class McTaskConfirmUpdateToRejectHandler implements McTaskStatusUpdateIHa
                 .setLineStatus(McTaskLineStatusEnum.NORMAL.getCode()));
 
         for (WipMcTaskLineView wipMcTaskLineView : wipMcTaskLineViews) {
-            if (!isCancelled(wipMcTaskLineView.getDeliveryOutLineStatus()) && !isReturnMaterial(wipMcTaskLineView)) {
+            if (validateService.hasDeliveryBill(wipMcTaskLineView.getDeliveryOutLineStatus()) && !validateService.isReturnMaterial(wipMcTaskLineView)) {
                 throw new ParamsIncorrectException("调拨单需已作废或已退料才可进行该操作");
             }
         }
     }
 
-    private boolean isCancelled(String status) {
-        return StringUtils.isNotBlank(status) && McTaskDeliveryStatusEnum.CANCELLED.getCode().equals(status);
-    }
 
-
-    private boolean isReturnMaterial(WipMcTaskLineView wipMcTaskLineView) {
-        return StringUtils.isNotBlank(wipMcTaskLineView.getDeliveryRmLineStatus())
-                && McTaskDeliveryStatusEnum.POSTED.getCode().equals(wipMcTaskLineView.getDeliveryRmLineStatus())
-                && StringUtils.isNotBlank(wipMcTaskLineView.getDeliveryRmLineSource())
-                && wipMcTaskLineView.getDeliveryRmLineSource().equals(wipMcTaskLineView.getDeliveryOutStockLineId());
-    }
 
 }
